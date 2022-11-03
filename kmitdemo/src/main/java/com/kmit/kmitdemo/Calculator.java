7db80576e326f7ae9b1962915f5b474e9701f002
unknown <shobithgampa1313@gmail.com>
2022-11-03 11:12:05 +0530

    maven first commit

---
+++ kmitdemo/src/main/java/com/kmit/kmitdemo/Calculator.java
@@ -0,0 +1,10 @@
+package com.kmit.kmitdemo;
+
+/**
+ * Simple calculator interface.
+ */
+public interface Calculator {
+    int add(int x, int y);
+    int multiply(int x, int y);
+    void save(int x);
+}
